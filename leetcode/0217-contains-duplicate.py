54bd22ffcc1acdfd1b883761f37e4909f60d4d37
sandro <assandroa95@gmail.com>
2023-01-01 11:01:20 +0700

    array contains duplicate

---
+++ leetcode/0217-contains-duplicate.py
@@ -0,0 +1,8 @@
+# array and hashing
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # use set to store unique element
+        s = set(nums)
+
+        # check if the length of set is smaller than the array (means contains duplicate)
+        return len(s) < len(nums)
