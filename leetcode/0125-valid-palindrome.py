de779f25e3a76f0859c92665e4f080b003e88159
sandro <assandroa95@gmail.com>
2023-01-13 09:14:12 +0700

    solve two sum II

---
+++ leetcode/0125-valid-palindrome.py
@@ -0,0 +1,23 @@
+# two pointer
+# TC: O(n)
+# SC: O(n)
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) == 1:
+            return True
+
+        left, right = 0, len(s) - 1
+
+        while left < right:
+            while not s[left].isalnum() and left < right:
+                left += 1
+
+            while not s[right].isalnum() and left < right:
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+
+        return True
