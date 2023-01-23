abb6e00c03c9dc07b01ddd8bc360242c79b5cf31
sandro <assandroa95@gmail.com>
2023-01-23 09:59:21 +0700

    improve algo

--- leetcode/0014-longest-common-prefix.py
+++ leetcode/0014-longest-common-prefix.py
@@ -1,22 +1,15 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        # handling edge case, when there is only one string
-        # the string itself is the common prefix
         if len(strs) == 1:
             return strs[0]
 
         last_common = 0
-        # iterate for each character in the first string
         for i in range(len(strs[0])):
             last_common = i
             curr_c = strs[0][i]
 
-            # iterate each string on a given character position
-            for s in strs:
-                # if string is shorter than other string
-                # or string have difference character
-                if i >= len(s) or s[i] != curr_c:
-                    return s[:last_common]
+            for j in range(len(strs)):
+                if i >= len(strs[j]) or strs[j][i] != curr_c:
+                    return strs[0][:last_common]
 
-        # if all passed than return the whole first string
-        return strs[0]
+        return strs[0][:last_common + 1]
