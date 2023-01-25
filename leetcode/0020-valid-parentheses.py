98850806ebcca78199e49ccc5a4284425be92ee6
sandro <assandroa95@gmail.com>
2023-01-25 10:00:21 +0700

    improve algo

--- leetcode/0020-valid-parentheses.py
+++ leetcode/0020-valid-parentheses.py
@@ -1,31 +1,18 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        checks = []
-        openTags = ['(', '{', '[']
-        closeTags = [')', '}', ']']
-        closeTagsPair = {
-            ')': '(',
-            '}': '{',
-            ']': '['
-        }
-
-        if len(s) == 1:
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if len(s) % 2 != 0:
             return False
 
+        d = {")": "(", "}": "{", "]": "["}
+
+        stack = []
+
         for c in s:
-            if c in openTags:
-                checks.append(c)
+            if not stack or (not c in d):
+                stack.append(c)
             else:
-                try:
-                    if c in closeTags and closeTagsPair[c] == checks[-1]:
-                        checks.pop()
-                    else:
-                        return False
-                except:
+                last = stack.pop()
+                if last != d[c]:
                     return False
 
-        return not bool(checks)
+        return not stack
