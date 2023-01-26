eba0af7aba604c387673282c144d437cac2dd7d4
sandro <assandroa95@gmail.com>
2023-01-26 08:01:21 +0700

    improve algo recursive -> iterative

--- leetcode/0021-merge-two-sorted-lists.py
+++ leetcode/0021-merge-two-sorted-lists.py
@@ -4,11 +4,8 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-
-        temp1 = list1
-        temp2 = list2
-        start = ListNode(0, None)
+    def mergeTwoLists(self, temp1: Optional[ListNode], temp2: Optional[ListNode]) -> Optional[ListNode]:
+        start = ListNode()
         temp = start
 
         while temp1 and temp2:
@@ -20,14 +17,10 @@ class Solution:
                 temp2 = temp2.next
             temp = temp.next
 
-        while temp1:
+        if temp1:
             temp.next = temp1
-            temp1 = temp1.next
-            temp = temp.next
 
-        while temp2:
+        if temp2:
             temp.next = temp2
-            temp2 = temp2.next
-            temp = temp.next
 
         return start.next
