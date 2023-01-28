e015096665bdd731ee00dc2af284a0635d61ed15
sandro <assandroa95@gmail.com>
2023-01-28 08:02:21 +0700

    improve algo

--- leetcode/0023-merge-k-sorted-lists.py
+++ leetcode/0023-merge-k-sorted-lists.py
@@ -9,15 +9,23 @@ class Solution:
             return None
 
         def mergeTwoList(l1, l2):
-            if not l1 or not l2:
-                return l1 if l1 else l2
+            dummy = ListNode()
+            temp = dummy
+            while l1 and l2:
+                if l1.val < l2.val:
+                    temp.next = l1
+                    l1 = l1.next
+                else:
+                    temp.next = l2
+                    l2 = l2.next
 
-            if l1.val < l2.val:
-                l1.next = mergeTwoList(l1.next, l2)
-                return l1
-            else:
-                l2.next = mergeTwoList(l1, l2.next)
-                return l2
+                temp = temp.next
+            if l1:
+                temp.next = l1
+            if l2:
+                temp.next = l2
+
+            return dummy.next
 
         res = lists.pop()
         while lists:
