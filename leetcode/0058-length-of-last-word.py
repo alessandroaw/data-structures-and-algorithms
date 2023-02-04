cc4fc3b774b1aa06e2db4a37809f37caad017e4e
sandro <assandroa95@gmail.com>
2023-02-04 08:04:20 +0700

    improve leetcode 58

--- leetcode/0058-length-of-last-word.py
+++ leetcode/0058-length-of-last-word.py
@@ -1,3 +1,16 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        return len(s.strip().split(" ")[-1])
+        isWord = False
+        start = 0
+        end = len(s)
+        for i in range(len(s)-1, -1, -1):
+            if isWord and s[i] == " ":
+                break
+
+            if s[i].isalnum():
+                if not isWord:
+                    isWord = True
+                    end = i + 1
+                start = i
+
+        return end - start
